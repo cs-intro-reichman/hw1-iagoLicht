a3b42b41e7ee691bec1309ffd5be9cba42b8700c
Guy Licht <lichtguy@gmail.com>
2024-11-07 15:33:20 +0200

    fix bill3 order

--- Bill3.java
+++ Bill3.java
@@ -2,17 +2,14 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
 		String value = args[3];
-		int valueInt = Integer.parseInt(value);
-		double valueDouble = valueInt;
-		double totalpay = Math.ceil(valueDouble/3);
-		System.out.println("dear " + name1 + ", "  + name2 + " and " + name3 + ": pay " + totalpay + " Shekels each.");
-		System.out.println("pay " + totalpay + " Shekels each.");
+		double valueDouble = Double.parseDouble(value);
+		double totalpay = Math.ceil(valueDouble / 3);
+		System.out.println("dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + totalpay + " Shekels each.");
 
-	
-	    // Replace this comment with the rest of your code
+		// Replace this comment with the rest of your code
 	}
 }
