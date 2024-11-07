4d12e811cd0affa7ded15a62708f78010967ed61
unknown <lichtguy@gmail.com>
2024-11-07 14:49:32 +0200

    fix bill3

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,16 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		String value = args[3];
+		int valueInt = Integer.parseInt(value);
+		double valueDouble = valueInt;
+		double totalpay = Math.ceil(valueDouble/3);
+		System.out.println("dear " + name1 + ", "  + name2 + " and " + name3 + ": pay " + totalpay + " Shekels each.");
+		System.out.println("pay " + totalpay + " Shekels each.");
+
+	
+	    // Replace this comment with the rest of your code
 	}
 }
