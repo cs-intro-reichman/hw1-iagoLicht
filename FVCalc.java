43adb041e6fdb8817a5dfedfd438de25c25133ab
Guy Licht <lichtguy@gmail.com>
2024-11-08 16:46:57 +0200

    FIRST CHECK E2

--- FVCalc.java
+++ FVCalc.java
@@ -3,15 +3,16 @@
 import javax.management.StringValueExp;
 
 public class FVCalc {
-	public static void main(String[] args){;
-	int currentValue = Integer.parseInt (args[0]);
-	Double rate = Double.parseDouble(args[1]);
-	int n = Integer.parseInt (args[2]);
-	double raten= (rate/100);
-	double FutureValue = currentValue*(Math.pow(1+raten,n));
-	System.out.println("After " +  "   " + n +  "years " + " ," + "$" + currentValue + "saved at " + rate + "% " + "will yield $" + FutureValue );
+	public static void main(String[] args) {
+		;
+		int currentValue = Integer.parseInt(args[0]);
+		Double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+		double raten = (rate / 100);
+		double FutureValue = currentValue * (Math.pow(1 + raten, n));
+		System.out.println("After " + "   " + n + "years " + " ," + "$" + currentValue + "saved at " + rate + "% "
+				+ "will yield $" + FutureValue);
 
-		
-	} 
+	}
 
 }
